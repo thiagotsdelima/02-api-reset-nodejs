c45510b4122f00d75af61197574fbe3e173bfa4e
thiagotsdelima <thiagotsdelima@outlook.com>
2024-05-24 13:01:51 -0300

    created BD
    created app.db

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,12 @@
 import setupKnex from 'knex';
 
-export const knex = setupKnex({
+export const config = {
   client: 'sqlite',
   connection: {
     filename: './tmp/app.db'
-  }
-});
+  },
+  useNullAsDefault: true,
+}
+
+
+export const knex = setupKnex(config);
