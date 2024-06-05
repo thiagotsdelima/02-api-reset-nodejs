5a0a6491eb397599256a29118ae8f20a1b5d3954
thiagotsdelima <thiagotsdelima@outlook.com>
2024-06-05 19:37:32 -0300

    created test

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,17 +1,23 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
   console.error('❌ Invalid environment variables:', _env.error.format())
-  throw new Error('Ivalid environment variables')
+  throw new Error('Invalid environment variables')
 }
 
-export const env = _env.data;
+export const env = _env.data
